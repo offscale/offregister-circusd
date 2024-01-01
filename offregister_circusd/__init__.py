f7a2ae1d3dcedfbb876b68951de89d67e4ffa022
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-01-01 18:23:55 -0500

    [setup.py] Fix Python 2.7 support ; standardise install scripts across all off* Python packages; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_circusd/__init__.py
+++ offregister_circusd/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 __author__ = "Samuel Marks"
 __version__ = "0.0.2-beta"
 __description__ = "Offregister implementation of Circus, to setup and serve python APIs"
